49feb633a67e171a9154670d31f861a200a0c7be
Ed Sweeney <ed@onextent.com>
2025-04-13 15:29:05 -0700

    fix tonic versions

--- crates/api/build.rs
+++ crates/api/build.rs
@@ -1,6 +1,6 @@
 fn main() -> Result<(), Box<dyn std::error::Error>> {
     tonic_build::configure()
         .build_server(true)
-        .compile(&["../../proto/analytics.proto"], &["../../proto"])?;
+        .compile_protos(&["../../proto/analytics.proto"], &["../../proto"])?;
     Ok(())
 }
